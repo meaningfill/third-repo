dc01e27af6447baca66cdbdd2b0e6e01070e18e9
meaningfill <whitecream2029@gmail.com>
2025-12-31 09:43:15 +0900

    Add buzz function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,13 @@ def fizz(n):
     if n % 3 == 0:
         return "Fizz"
     return str(n)
+
+def fizz(n):
+    if n % 3 == 0:
+        return "Fizz"
+    return str(n)
+
+def buzz(n):
+    if n % 5 == 0:
+        return "Buzz"
+    return str(n)
